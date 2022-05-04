17209779aa0ec739a2a6d17973fae10267cdfda2
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-05-04 20:11:19 +0600

    adding get post request

--- index.js
+++ index.js
@@ -4,10 +4,52 @@ require('dotenv').config();
 const cors = require('cors');
 const { use } = require('express/lib/router');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.ou2fz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () =>{
+    try{
+
+        await client.connect()
+        const productCollection = client.db("productsCollection").collection("product")
+
+        // adding data to server
+
+        app.post('/product', async(req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
+
+        // get data from server
+
+        app.get('/product', async(req, res)=> {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+        })
+
+
+
+
+    }
+    finally{
+        // client.close()
+    }
+}
+
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
